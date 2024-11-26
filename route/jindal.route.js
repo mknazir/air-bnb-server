3fba751c85629852278508435489bdac04b66112
Aaquib <aaquibm502@gmail.com>
2024-11-26 15:03:53 +0530

    verify order

--- route/jindal.route.js
+++ route/jindal.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { jindalCreateCourse, getjindalCourseDetails } = require('../controller/jndal.controller');
+const { jindalCreateCourse, getjindalCourseDetails, createOrder, verifyOrder } = require('../controller/jndal.controller');
 const jindalRouter = express.Router();
 jindalRouter.post('/course/jindalcreateCourse', jindalCreateCourse);
 jindalRouter.get('/course/jindalcoursedetails/:courseId', getjindalCourseDetails);
+jindalRouter.post('/jindal/createOrder', createOrder);
+jindalRouter.post('/jindal/verifyOrder',verifyOrder)
 module.exports =jindalRouter;
 
   
